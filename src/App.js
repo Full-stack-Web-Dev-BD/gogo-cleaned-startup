6f16a833b56b7f8559a6c49adf63a1773324fc80
unknown <Alamin>
2021-08-22 19:53:52 +0600

    Done

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { Suspense } from 'react';
+import { connect } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
+import './helpers/Firebase';
+import AppLocale from './lang';
+import ColorSwitcher from './components/common/ColorSwitcher';
+import { NotificationContainer } from './components/common/react-notifications';
+import {
+  isMultiColorActive,
+  adminRoot,
+  UserRole,
+} from './constants/defaultValues';
+import { getDirection } from './helpers/Utils';
+import { ProtectedRoute } from './helpers/authHelper';
+
+const Dashboard = React.lazy(() =>
+  import(/* webpackChunkName: "views-app" */ './views/app')
+);
+const ViewHome = React.lazy(() =>
+  import(/* webpackChunkName: "views" */ './views/home')
+);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    const direction = getDirection();
+    if (direction.isRtl) {
+      document.body.classList.add('rtl');
+      document.body.classList.remove('ltr');
+    } else {
+      document.body.classList.add('ltr');
+      document.body.classList.remove('rtl');
+    }
+  }
+
+  render() {
+    const { locale } = this.props;
+    const currentAppLocale = AppLocale[locale];
+
+    return (
+      <div className="h-100">
+        <IntlProvider
+          locale={currentAppLocale.locale}
+          messages={currentAppLocale.messages}
+        >
+          <>
+            <NotificationContainer />
+            {isMultiColorActive && <ColorSwitcher />}
+            <Suspense fallback={<div className="loading" />}>
+              <Router>
+                <Switch>
+                  <ProtectedRoute
+                    path={adminRoot}
+                    component={Dashboard}
+                    roles={[UserRole.Admin, UserRole.Editor]}
+                  />
+                  <Route
+                    path="/"
+                    exact
+                    render={(props) => <ViewHome {...props} />}
+                  />
+                  <Redirect to="/error" />
+                </Switch>
+              </Router>
+            </Suspense>
+          </>
+        </IntlProvider>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ authUser, settings }) => {
+  const { currentUser } = authUser;
+  const { locale } = settings;
+  return { currentUser, locale };
+};
+const mapActionsToProps = {};
+
+export default connect(mapStateToProps, mapActionsToProps)(App);
